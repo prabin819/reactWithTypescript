86fc4de8df07e586c4f7a80e04a150c0ad4d2444
Prabin <manandharprabin819@gmail.com>
2024-07-16 01:44:45 +0545

    15 => useContext Future Value

---
+++ src/components/context/User.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+export const User = () => {
+  const userContext = useContext(UserContext);
+  const handleLogin = () => {
+    if (userContext) {
+      userContext.setUser({
+        name: "viswas",
+        email: "viswas@male.com",
+      });
+    }
+  };
+  const handleLogout = () => {
+    if (userContext) {
+      userContext.setUser(null);
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <br />
+      <br />
+      <hr />
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext?.user?.name}</div>
+      <div>User email is {userContext?.user?.email}</div>
+    </div>
+  );
+};
