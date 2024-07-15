26b8bea629c46e92168fcb6d78b53e1dd3d01281
Prabin <manandharprabin819@gmail.com>
2024-07-15 17:34:43 +0545

    6 => event props => function as props without parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
-import Greet from "./components/Greet";
-import { Heading } from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import { Status } from "./components/Status";
+import { Button } from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <Status status="error" />
-      <Heading>placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name="viswas" messageCount={30} isLoggedIn={true} />
+      <Button
+        handleClick={() => {
+          alert("button clicked");
+        }}
+      />
     </div>
   );
 }
