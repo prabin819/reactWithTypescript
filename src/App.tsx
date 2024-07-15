4d019baa6c74d16234bf377fe4f9d6d069c95126
Prabin <manandharprabin819@gmail.com>
2024-07-15 22:37:10 +0545

    7 => style props => styles: React.CSSProperties

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Container } from "./components/Container";
 import { Input } from "./components/Input";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         }}
       />
       <Input value="" handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
     </div>
   );
 }
