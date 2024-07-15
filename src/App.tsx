f1adaa384adb1a7ed7615f563b047619a9381097
Prabin <manandharprabin819@gmail.com>
2024-07-16 01:07:20 +0545

    14 => useContext (same as in js or jsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Container } from "./components/Container";
-import { Input } from "./components/Input";
-import { Counter } from "./components/state/Counter";
-import { LoggedIn } from "./components/state/LoggedIn";
+import { ThemeContextProvider } from "./components/context/ThemeContext";
+import { Box } from "./components/context/Box";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => {
-          console.log("button clicked", event, id);
-        }}
-      />
-      <Input value="" handleChange={(event) => console.log(event)} />
-      <Container styles={{ border: "1px solid black", padding: "1rem" }} />
-      <LoggedIn />
-      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
