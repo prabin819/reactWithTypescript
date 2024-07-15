aa8c6a234d2ec03526b3292019bd4938b3f565c8
Prabin <manandharprabin819@gmail.com>
2024-07-16 00:27:48 +0545

    12 => useReducer Hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Button } from "./components/Button";
 import { Container } from "./components/Container";
 import { Input } from "./components/Input";
+import { Counter } from "./components/state/Counter";
 import { LoggedIn } from "./components/state/LoggedIn";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Input value="" handleChange={(event) => console.log(event)} />
       <Container styles={{ border: "1px solid black", padding: "1rem" }} />
       <LoggedIn />
+      <Counter />
     </div>
   );
 }
