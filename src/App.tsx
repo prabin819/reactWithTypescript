534f495246236a342d46ca253ad72a3906967c08
Prabin <manandharprabin819@gmail.com>
2024-07-15 17:48:49 +0545

    6 => event props => onclick and onchange events => handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import { Button } from "./components/Button";
+import { Input } from "./components/Input";
 
 function App() {
   return (
     <div className="App">
       <Button
-        handleClick={(event) => {
-          console.log("button clicked", event);
+        handleClick={(event, id) => {
+          console.log("button clicked", event, id);
         }}
       />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
 type ButtonProps = {
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>, id: number) => void;
 };
 
 export const Button = (props: ButtonProps) => {
-  return <button onClick={props.handleClick}>Click</button>;
+  return (
+    <button onClick={(event) => props.handleClick(event, 1)}>Click</button>
+  );
 };
